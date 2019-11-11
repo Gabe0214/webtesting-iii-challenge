d218adc3a6de8d6ca25ca59804ac6022569357ef
Gabriel Anguiano <anguianog97@gmail.com>
2019-11-11 13:17:23 -0800

    snapshot for controls and initial commit

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+
+import Controls from './Controls'
+
+test('Controls render correctly', () => {
+    expect(render(<Controls/>)).toMatchSnapshot()
+}); 
+
+
+
+
